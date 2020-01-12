42e4d9f7e55265c9c7a0f060501b4a7732131508
Bogusz Jelinski <bogusz.jelinski@gmail.com>
2020-01-12 16:58:19 +0100

    spring starter

---
+++ src/main/java/com/kapsoola/kaboot/KabootApplication.java
@@ -0,0 +1,13 @@
+package com.kapsoola.kaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapsoola/kaboot/KabootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapsoola.kaboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
